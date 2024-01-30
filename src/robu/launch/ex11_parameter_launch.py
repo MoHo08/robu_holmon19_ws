cc2c0df46e53c14c061107df2db010c345db8873
MoHo08 <holmon19@htl-kaindorf.at>
2024-01-30 10:53:28 +0100

    ex11 lauch Änderung

--- src/robu/launch/ex11_parameter_launch.py
+++ src/robu/launch/ex11_parameter_launch.py
@@ -1,15 +1,5 @@
-import os
 import launch
 import launch_ros.actions
-from launch.actions.execute_process import ExecuteProcess
-from ament_index_python.packages import get_package_share_directory
-from launch.substitutions import LaunchConfiguration
-
-from launch.substitutions import LaunchConfiguration
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir
-
 
 def generate_launch_description():
 
