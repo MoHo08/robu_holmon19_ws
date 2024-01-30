565b0090d5bf89f8d4d9be3ca44c1565da33530f
MoHo08 <holmon19@htl-kaindorf.at>
2024-01-30 10:12:20 +0100

    ex11 launch datein debug

--- src/robu/launch/ex11_parameter_launch.py
+++ src/robu/launch/ex11_parameter_launch.py
@@ -1,9 +1,21 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
+import os
+import launch
+import launch_ros.actions
+from launch.actions.execute_process import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
 
-def gernerate_launch_description():
-    return LaunchDescription([
-        Node(
+from launch.substitutions import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
+
+
+def generate_launch_description():
+
+    return launch.LaunchDescription([
+
+        launch_ros.actions.Node(
             package='robu',
             executable='myparameter',
             name='custom_minimal_Param_node',
