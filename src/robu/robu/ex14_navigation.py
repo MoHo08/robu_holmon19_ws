f0c65ca2228399d29eb72180cfde843ceecd98b2
MoHo08 <holmon19@htl-kaindorf.at>
2024-04-09 12:54:40 +0200

    CLI-Kommands navigieren

--- src/robu/robu/ex14_navigation.py
+++ src/robu/robu/ex14_navigation.py
@@ -1,2 +1,5 @@
 # CLI-Commands
 #1. action list: ros2 action list
+#2. action info: ros2 action info <action info> -t
+#3. interface list: ros2 interface show /nav2_msgs/action/NavigateToPose
+#4. action call: ros2 action send_goal /navigate_to_pose nav2_msgs/action/NavigateToPose "{pose: {header: {frame_id: 'map'}, pose: {position: {x: 3.75, y: 1.0, z: 0.01}, orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}}}" 
